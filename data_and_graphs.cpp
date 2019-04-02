8beeaabeac54b05e077a804742641f3678acadc3
Nicole <nicolelhagerman@gmail.com>
2019-04-02 13:21:52 -0600

    comments and modification of inputs

--- data_and_graphs.cpp
+++ data_and_graphs.cpp
@@ -17,15 +17,6 @@ using namespace std;
 
 #define ll long long
 
-// typedef pair<float, float> pff;
-
-// struct flight {
-//   int32_t start_lat;
-//   int32_t start_lon;
-//   int32_t end_lat;
-//   int32_t end_lon;
-// };
-
 void readAirports(unordered_map<string, airport>& airports, string filename) {
     ifstream file;
     string name,start, line, str_read;
@@ -43,39 +34,39 @@ void readAirports(unordered_map<string, airport>& airports, string filename) {
 
     while (getline(file, line)) {
         airport currentAirport;
+
+        // read the id of the airport
         str_start = 0;
         str_end = line.find(",", str_start+1);
-        // cout << "START END " << str_start << "  " << str_end << endl;
         str_read = line.substr(str_start, str_end-str_start);
         int32_t id = stoi(str_read);
         currentAirport.id = id;
 
-        // cout << "LINE: " << line << endl;
-        // cout << "  ID: " << str_read;
+        // read the 3 digit name of the airport
         str_end = line.find(",", str_start+1);
         for (int i = 0; i < 4; i++) {
             str_start = line.find(",", str_start+1);
         }
-
         name = line.substr(str_start+1, 3);
         currentAirport.name = name;
-        // cout << "  NAME: " << name;
         
+        // read the lat of the airport
         for (int i = 0; i < 2; i++) {
             str_start = line.find(",", str_start+1);
         }
-
         str_end = line.find(",", str_start+1);
         str_read = line.substr(str_start+1, str_end-str_start-1);
         float lat = stof(str_read);
         currentAirport.lat = lat;
 
+        // read the lon of the airport
         str_start = line.find(",", str_start+1);
         str_end = line.find(",", str_start+1);
         str_read = line.substr(str_start+1, str_end-str_start-1);
         float lon = stof(str_read);
         currentAirport.lon = lon;
 
+        // insert airport into hash table
         airports.insert({name, currentAirport});
     }
     file.close();
@@ -83,7 +74,7 @@ void readAirports(unordered_map<string, airport>& airports, string filename) {
 
 void readFlights(unordered_map<string, ll>& flights, const vector<string>& destinations, string filename, unordered_map<string, airport>& airports) {
     ifstream file;
-    string path, start, end, line, str_read;
+    string path, start, end, line;
     int str_start, str_end;
   
     file.open(filename);
@@ -97,23 +88,30 @@ void readFlights(unordered_map<string, ll>& flights, const vector<string>& desti
     getline(file, line);
 
     while (getline(file, line)) {
+
+        // gets start aiport
         str_start = 0;
         for (int i = 0; i < 2; i++) {
             str_start = line.find(",", str_start+1);
         }
         str_end = line.find(",", str_start+1);
         start = line.substr(str_start+1, str_end-str_start-1);
+        
+        // checks if the start destination of the flight is in the user inputted list
+        if(find(destinations.begin(), destinations.end(), start) != destinations.end()) {
+
+        // gets ending airport
         str_start = line.find(",", str_end+1);
         str_end = line.find(",", str_start+1);
         end = line.substr(str_start+1, str_end-str_start-1);
-        if(find(destinations.begin(), destinations.end(), start) != destinations.end()) {
-            if (find(destinations.begin(), destinations.end(), end) != destinations.end()) {
 
+            // checks if the end destination of the flight is in the user inputted list
+            if (find(destinations.begin(), destinations.end(), end) != destinations.end()) {
                 path = start + end;
+
+                // Makes sure that starting and ending airports exist
                 auto search_start = airports.find(start);
                 auto search_end = airports.find(end);
-                
-
                 if (search_start != airports.end() && search_end != airports.end()) {
                     auto search_flight = flights.find(path);                 
                     if (search_flight == flights.end()) {
@@ -148,30 +146,33 @@ int main() {
 
     
     /* Read in user list of airports and create a vector from the airports */
-    // vector<string> destinations;
-    // int n;
-    // cout << "How many destinations would you like to visit? ";
-    // cin >> n;
-    // cout << "Enter a list of airports (3): ";
-    // for (int i = 0; i<n; ++i) {
-    //     cin >> a;
-    //     destinations.push_back(a);
-    // }
-    // // output the list of airports
-    // cout << "The list of airports is: ";
-    // for (vector<string>::const_iterator i = destinations.begin(); i != destinations.end(); ++i) {
-    //     cout << *i << ' ';
-    // }
-    // cout << endl;
+    vector<string> destinations;
+    int n;
+    cout << "How many destinations would you like to visit? ";
+    cin >> n;
+    cout << "Enter a list of airports: ";
+    for (int i = 0; i<n; ++i) {
+        cin >> a;
+        destinations.push_back(a);
+    }
+    cout << endl;
 
     /* Read in Flights to a Hash table */
-    vector<string> destinations;
-    destinations.push_back("YEG"); 
-    destinations.push_back("YVR"); 
-    destinations.push_back("YYC");
-    cout << "The list of airports is: ";
-    for (vector<string>::const_iterator i = destinations.begin(); i != destinations.end(); ++i) {
-        cout << *i << ' ';
+    auto it = destinations.begin();
+    while (it != destinations.end()){
+        auto exists = airports.find(*it);
+        if (exists == airports.end()) {
+            cout << *it << ": Airport not found and was removed from list" << endl;
+            it = destinations.erase(it);
+            --n;
+        } else {
+            cout << *it << ": Airport exists and is okay" << endl; 
+            ++it;
+        }
+    }
+    cout << "There are " << n << " airports: ";
+    for (auto i = destinations.begin(); i != destinations.end(); ++i) {
+        cout << *i << " ";
     }
     cout << endl;
     string flight_data = "data/flightData.csv";
@@ -179,7 +180,7 @@ int main() {
     readFlights(flights, destinations, flight_data, airports);
     string start,end;
     cout << "Enter a starting airport for the flight: ";
-    cin >> start;    
+    cin >> start;
     cout << "Enter an end airport for the flight: ";
     cin >> end;
     a = start + end;
