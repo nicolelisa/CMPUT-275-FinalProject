2ca9ca1b251a9b51a5ca02bdcda7d4b5eca867cf
riona-wiberg <riona@ualberta.ca>
2019-04-02 11:17:47 -0600

    nn changes

--- nn.cpp
+++ nn.cpp
@@ -1,9 +1,16 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
+#include <string>
+#include "wdigraph.h"
 
 using namespace std;
 
-// void nearestNeighbour() {
+// WDigraph createGraph(vector<string> namesOfAirports) {
+//     WDigraph airportGraph;
     
+// }
+
+// vector<string> nearestNeighbour(WDigraph& airportGraph, ) {
+
 // }
