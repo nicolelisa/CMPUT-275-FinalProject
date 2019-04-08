eeb1c0734cdac298cb4268e3888e25b52a2b15e3
Nicole <nicolelhagerman@gmail.com>
2019-04-07 21:15:27 -0600

    comments and fileheader to airport.h

--- airport.h
+++ airport.h
@@ -1,7 +1,12 @@
 /*
-  Contains the restaurant struct declaration, functions to read restaurants
-  from the SD card, and functions to sort a restaurant around a given
-  latitude/longitude
+Name: Nicole Hagerman
+Student ID: 1200596
+
+Name: Riona Wiberg
+Student ID: 1532156
+
+CMPUT 275, Winter 2019
+Project: Flightpath
 */
 
 #ifndef _AIRPORT_H_
@@ -10,58 +15,48 @@
 #include <iostream>
 #include "airport.h"
 
-// https://rosettacode.org/wiki/Haversine_formula
 using namespace std;
 
-const static double EarthRadiusKm = 6372.8;
- 
-inline double DegreeToRadian(double angle)
-{
-	return M_PI * angle / 180.0;
-}
- 
-class Coordinate
-{
-public:
-	Coordinate(double latitude ,double longitude):myLatitude(latitude), myLongitude(longitude)
-	{}
- 
-	double Latitude() const
-	{
-		return myLatitude;
-	}
- 
-	double Longitude() const
-	{
-		return myLongitude;
-	}
- 
-private:
- 
-	double myLatitude;
-	double myLongitude;
-};
- 
-long long HaversineDistance(const Coordinate& p1, const Coordinate& p2)
-{
-	double latRad1 = DegreeToRadian(p1.Latitude());
-	double latRad2 = DegreeToRadian(p2.Latitude());
-	double lonRad1 = DegreeToRadian(p1.Longitude());
-	double lonRad2 = DegreeToRadian(p2.Longitude());
- 
-	double diffLa = latRad2 - latRad1;
-	double doffLo = lonRad2 - lonRad1;
- 
-	double computation = asin(sqrt(sin(diffLa / 2) * sin(diffLa / 2) + cos(latRad1) * cos(latRad2) * sin(doffLo / 2) * sin(doffLo / 2)));
-	return 2 * EarthRadiusKm * computation;
-}
- 
-
 struct airport {
-  string name; // Index of flight from 0 to NUM_FLIGHTS-1.
-  int id; // Index of restaurant from 0 to NUM_FLIGHTS-1.
+  string name; // Name of airport (3 letter naming convention)
+  int id; // ID of the airport
   float lat;
   float lon;
 };
+
+// https://rosettacode.org/wiki/Haversine_formula
+const static double EarthRadiusKm = 6372.8;
+inline double DegreeToRadian(double angle) {
+	return M_PI * angle / 180.0;
+}
+ 
+class Coordinate {
+	public:
+		Coordinate(double latitude ,double longitude):myLatitude(latitude), myLongitude(longitude) {}
+	 	double Latitude() const {
+			return myLatitude;
+		}
+		double Longitude() const {
+			return myLongitude;
+		}
+
+	private:
+		double myLatitude;
+		double myLongitude;
+	};
+
+// Calculates the distance using coordinate points
+long long HaversineDistance(const Coordinate& p1, const Coordinate& p2) {
+		double latRad1 = DegreeToRadian(p1.Latitude());
+		double latRad2 = DegreeToRadian(p2.Latitude());
+		double lonRad1 = DegreeToRadian(p1.Longitude());
+		double lonRad2 = DegreeToRadian(p2.Longitude());
+	 
+		double diffLa = latRad2 - latRad1;
+		double doffLo = lonRad2 - lonRad1;
+	 
+		double computation = asin(sqrt(sin(diffLa / 2) * sin(diffLa / 2) + cos(latRad1) * cos(latRad2) * sin(doffLo / 2) * sin(doffLo / 2)));
+		return 2 * EarthRadiusKm * computation;
+}
 
 #endif
