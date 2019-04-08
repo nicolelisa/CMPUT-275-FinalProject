92939fd4d5f065951d105400cabaef735706e314
Nicole <nicolelhagerman@gmail.com>
2019-04-07 21:00:26 -0600

    clean up code and remove no path found print

--- bruteforce.h
+++ bruteforce.h
@@ -32,28 +32,20 @@ void findPermutations(vector<string>& dest, vector<vector<string>>& perms, int l
 void findPath(vector<string>& currentPerm, int& position, ll& currentDist, int start, int end, const WDigraph& pathGraph, const unordered_map<int, string>& idsToAirports) {
 	unordered_map<int, PIL> tree;
 	dijkstra(pathGraph, start, tree);
-	  if (tree.find(end) == tree.end()) {
-	      cout << "No path found" << endl;
-	      currentDist = -1;
-		}  else {
-			currentDist += tree.find(end)->second.second;
-			// cout << "DISTANCE IS: " << tree.find(end)->second.second << endl;
+	if (tree.find(end) == tree.end()) {
+		// if there is no path that exists, set the distance of the permutation to 0
+		currentDist = -1;
+	} else {
+		currentDist += tree.find(end)->second.second;
 	    // read off the path by stepping back through the search tree
 	    list<int> path;
 	    while (end != start) {
 	    	path.push_front(end);
-			// auto search = idsToAirports.find(end);
-			// if (search != idsToAirports.end()) {
-			// 	currentPerm.insert(currentPerm.begin()+position, search->second);
-			// }
-	    	// cout << end << " " << search->second << endl;
 	    	end = tree[end].first;
 	    }
 	    path.pop_back();
-	    
-	    // position = 1;
-	    // cout << " LIST SIZE IS: " << path.size() << endl;
-	    // path.push_front(start);
+
+	    // search for airport name and add to the current permutation
 	    string st, end;
 	    auto search = idsToAirports.find(start);
 	    st = search->second;
@@ -61,19 +53,15 @@ void findPath(vector<string>& currentPerm, int& position, ll& currentDist, int s
 	    	auto search = idsToAirports.find(it);
 	    	string destName =  "*" + (search->second);
 	    	currentPerm.insert(currentPerm.begin()+position+1, destName);
-        	// cout << "  Layover added at: " << destName << endl;
-      	}
-      	position += path.size();
-	  }
-
+	  	}
+	  	position += path.size();
+	}
 }
 
 void bruteforce(vector<string> dest, const unordered_map<string, airport>& airports, const unordered_map<string, ll>& flights, 
 				const WDigraph pathGraph, const unordered_map<int, string>& idsToAirports) {
-
     vector<string> min_path = dest;
     ll min_distance = 0;
-
 	string home = dest.front();
 	dest.erase(dest.begin());
 	int count = dest.size();
@@ -81,48 +69,43 @@ void bruteforce(vector<string> dest, const unordered_map<string, airport>& airpo
     findPermutations(dest, perms, 1, count, home);
     int perm_count = perms.size();
 
-    // cout << "Permutations: " << perm_count << endl;
     for (int i=0; i<perm_count; ++i) {
-    	// cout << "====== Starting new iteration =====" << endl;
     	ll dist = 0;
-
-		// if path exists, get distance
+    	// if path exists, get distance and add to the distance total
 		for (int j=0; j<perms[i].size()-1; ++j) {
 			auto search = flights.find(perms[i][j] + perms[i][j+1]);
 			if (search != flights.end()) {
-				// cout << "Distance between " << perms[i][j] << " and " << perms[i][j+1] << " is " << search->second << endl;
 				dist += search->second;
 			} else {
-				// cout << perms[i][j] << " to " << perms[i][j+1] << " is not found" << endl;
-				// get ids of values
+				// get ids of start and end airport
 				int start_id, end_id;
 				auto search_start = airports.find(perms[i][j]);
         		if (search_start != airports.end()) {
             		start_id = search_start->second.id;
         		} else {
-        			// cout << perms[i][j] << " is not found" << endl;
-        			start_id = -1;
+        			dist = -1;
         		}
 		        auto search_end = airports.find(perms[i][j+1]);
 		        if (search_end != airports.end()) {
 		            end_id = search_end->second.id;
 		        } else {
-        			// cout << perms[i][j+1] << " is not found" << endl;
-        			end_id = -1;
+        			dist = -1;
         		}
-				findPath(perms[i], j, dist, start_id, end_id, pathGraph, idsToAirports);
-				if (dist == -1) {
-					// cout << "No path found" << endl;
+        		if (dist == -1) {
 					break;
 				}
+				findPath(perms[i], j, dist, start_id, end_id, pathGraph, idsToAirports);
 			}
 		}
-    	// cout << "Distance of iteration is: " << dist << endl << endl;
+
+    	// check if this is the shortest distance so far and if it is, set as min path
     	if (min_distance <= 0 || dist < min_distance) {
     		min_distance = dist;
     		min_path = perms[i];
     	}
     }
+
+    // print the shortest path to the console
     cout << endl << endl << "=========== BRUTE FORCE FLIGHTPATH COMPLETE ===========" << endl;
 	if (min_distance == -1) {
 		cout << "No path was found" << endl;
